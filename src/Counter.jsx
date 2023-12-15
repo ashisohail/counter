6bc5f8d1a9e74e7ac8d40a3935c90b5fe0b4506f
Ayesha <pu_ash@yahoo.com>
2023-12-15 18:29:44 +0000

    Updated store, counter and app component.

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { increment, decrement } from "./redux/actions";
 
-const Counter = ({ count, increment, decrement }) => {
+const Counter = ({ increment, decrement }) => {
+  const count = useSelector((state) => state.counter.count);
   return (
     <div>
       <h2>My Redus Counter</h2>
       <h3>Count: {count} </h3>
-      <button onclick={increment}>Increment</button>
+      <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
   );
@@ -24,4 +25,4 @@ const mapDispatchToProp = {
   decrement,
 };
 
-export default connect(mapStateToProps, mapDispatchToProp)(counter);
+export default connect(mapStateToProps, mapDispatchToProp)(Counter);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
-// import { configureStore } from "@redusjs/toolkit";
-import * as toolkitRaw from "@reduxjs/toolkit";
-const { configureStore } = toolkitRaw.default ?? toolkitRaw;
+import { configureStore } from "@reduxjs/toolkit";
+
 import counterReducer from "./reducers";
 
-const store = configureStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
 export default store;
